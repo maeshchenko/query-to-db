5b32fd3a6a78b7387107621e8cb6ecec02abbb0f
Mikhail Eshchenko <maeshenko@domclick.ru>
2020-11-14 17:22:17 +0300

    creating connectDB

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const connectDB = require('./config/db');
 
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
